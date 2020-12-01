ea953eddca73c3a10ef8db912ec4c4bc6235bd62
Scott Savage <ssavage@gmail.com>
2020-12-01 15:34:27 -0500

    update additional install dependencies

--- setup.py
+++ setup.py
@@ -20,6 +20,9 @@ setuptools.setup(
 
     install_requires=[
         "aws-cdk.core==1.75.0",
+        "aws_cdk.aws_apigateway==1.75.0",
+        "aws_cdk.aws_lambda==1.75.0",
+        "aws_cdk.aws_s3==1.75.0"
     ],
 
     python_requires=">=3.6",
